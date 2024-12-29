96168ebe49a525e7f414cd746099c717e0889b31
Alen Sunny <alenmontu007@gmail.com>
2024-12-29 21:41:36 +0530

    finished brainwave

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './components/Hero'
 import Benefits from './components/Benefits'
 import Collaboration from './components/Collaboration'
 import Services from './components/Services'
+import Pricing from './components/Pricing'
+import Roadmap from './components/Roadmap'
+import Footer from './components/Footer'
  const App=() => {
 
   return (
@@ -15,6 +18,9 @@ import Services from './components/Services'
     <Benefits/>
     <Collaboration/>
     <Services/>
+    <Pricing/>
+    <Roadmap/>
+    <Footer/>
    </div>
    <ButtonGradient/>
    </>
